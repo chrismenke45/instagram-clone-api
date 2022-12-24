3e35f97dd978a77422f6706450674691a328ecba
chrismenke45 <chrismenke45@gmail.com>
2022-12-23 16:07:21 -0800

    controllers/routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :posts, only: [:index, :show, :create, :destroy] do
+    resources :comments, only: [:index, :create, :destory]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+  root "posts#index"
 end
