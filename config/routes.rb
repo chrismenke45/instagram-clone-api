b2f84b4821c2e6943caee7ee48305836cf0734e5
chrismenke45 <chrismenke45@gmail.com>
2022-12-26 15:59:46 -0800

    adjusted likes routes to be nestedin posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'likes/create'
-  get 'likes/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :comments, only: [:index, :create, :destory]
+    resources :likes, only: [:create, :destroy]
   end
   # Defines the root path route ("/")
   # root "articles#index"
