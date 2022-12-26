a7456b5d802f38a464520b12deebfd75051e3d51
chrismenke45 <chrismenke45@gmail.com>
2022-12-26 08:58:43 -0800

    added my boilerplate to controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,41 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[ show destroy ]
+
   def index
+    @posts = post.order(created_at: :desc).limit(10)
+    render :json => @posts
   end
 
   def create
+    @post = Post.new(post_params)
+    #@post = User.posts.new(post_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
   def show
+    render :json => @post
   end
 
   def destroy
+    if @post.destory
+      render json: { message: "Post deleted" }
+    else
+      render json: @post.errors, status: :unprocessable_entity
+    end
   end
 
-  def record_params
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
     params.require(:post).permit(:picture_url, :caption)
   end
 end
