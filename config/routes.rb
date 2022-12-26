87a3769c12ac6a09187367d72ce598d039f76d66
chrismenke45 <chrismenke45@gmail.com>
2022-12-26 15:47:56 -0800

    generated likes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'likes/create'
+  get 'likes/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :comments, only: [:index, :create, :destory]
