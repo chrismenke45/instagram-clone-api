a57597fec3000c1f806e0f8dbcd56516cf0f2170
chrismenke45 <chrismenke45@gmail.com>
2022-12-26 20:55:47 -0800

    typo fix 'destory'

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
   validates :caption, length: { maximum: 2200 }
 
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destory
+  has_many :likes, dependent: :destroy
 
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destory
+  has_many :likes, dependent: :destroy
 end
