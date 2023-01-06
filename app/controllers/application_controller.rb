dcb3c7398431451ba3cd323ecb670183eda8f6af
chrismenke45 <chrismenke45@gmail.com>
2023-01-06 14:53:49 -0800

    jwt encode/decode/authenticate_user added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include JwtToken
   before_action :cors_set_access_control_headers
 
   private
@@ -10,5 +11,18 @@ class ApplicationController < ActionController::API
     headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE, GET, OPTIONS"
     headers["Access-Control-Request-Method"] = "*"
     headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  end
+
+  def authenticate_user
+    header = request.headers["Authorization"]
+    header = header.split(" ").last if header
+    begin
+      @decoded = JwtToken.decode(header)
+      @current_user = User.find(@decoded[:user_id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
   end
 end
