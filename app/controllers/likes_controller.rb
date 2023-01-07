64d30230e496db3c127cb94d893b06d0c6fe9ff9
chrismenke45 <chrismenke45@gmail.com>
2023-01-07 12:05:32 -0800

    likes controller boilerplate

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,27 @@
 class LikesController < ApplicationController
+  def index
+    @likes = Like.joins(:user).select("user.username, user.name, post.user_id").where("like.post_id = ?", params[:post_id])
+    render :json => @likes
+  end
+
   def create
     @like = Like.new(post_id: params[:post_id])
+    @like.user_id = @current_user.id
+    if @like.save
+      render :json => @like
+    else
+      render json: @like.errors, status: :unprocessable_entity
+    end
     #@like.user_id = current_user.id
 
   end
 
   def destroy
-    @like = Like.find(params[:id])
-    @like.destory
+    @like = Like.find_by(post_id: params[:post_id], user_id: @current_user.id)
+    if @like.destory
+      render :json => { message: "Like Destroyed" }
+    else
+      render :json => { error: "Like could not be destroyed" }, status: :unprocessable_entity
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show, :create, :destroy] do
     resources :comments, only: [:index, :create, :destory]
-    resources :likes, only: [:create, :destroy]
+    resources :likes, only: [:index, :create, :destroy]
   end
   resources :users, only: [:create, :show, :update, :destroy]
   post "auth/login", to: "authentications#login"
