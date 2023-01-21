ae2699f01884eb5b265b6c81c9ee084646e73113
chrismenke45 <chrismenke45@gmail.com>
2023-01-21 12:12:16 -0800

    follows model added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,14 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  # follower_follows "names" the Follow join table for accessing through the follower association
+  has_many :follower_follows, foreign_key: :followee_id, class_name: "Follow", dependent: :destroy
+  # source: :follower matches with the belong_to :follower identification in the Follow model
+  has_many :followers, through: :follower_follows, source: :follower
+
+  # followee_follows "names" the Follow join table for accessing through the followee association
+  has_many :followee_follows, foreign_key: :follower_id, class_name: "Follow", dependent: :destroy
+  # source: :followee matches with the belong_to :followee identification in the Follow model
+  has_many :followees, through: :followee_follows, source: :followee
 end
