b47423354770df1e8ba7a89493053286addfa37b
chrismenke45 <chrismenke45@gmail.com>
2023-01-21 18:45:09 -0800

    added custom validator to prevent user from following themself

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,6 +1,14 @@
+class FollowValidator < ActiveModel::Validator
+  def validate(record)
+    if record.followee_id == record.follower_id
+      record.errors.add :follower, "You cannot follow yourself!"
+    end
+  end
+end
+
 class Follow < ApplicationRecord
-  # belongs_to :follower
-  # belongs_to :followee
+  validates_with FollowValidator
+
   belongs_to :follower, foreign_key: "follower_id", class_name: "User"
   belongs_to :followee, foreign_key: "followee_id", class_name: "User"
 end
