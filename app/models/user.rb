e31f851337e82303fcfce63388c8b9ffe6591d72
chrismenke45 <chrismenke45@gmail.com>
2023-02-01 17:07:27 -0800

    added validator to only allow lowercase letters and numbers in username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true, length: { in: 3..30 }
-  validates :username, presence: true, uniqueness: true, length: { in: 3..30 }
+  validates :username, presence: true, format: { with: /\A[a-z0-9]+\z/ }, uniqueness: true, length: { in: 3..30 }
   validates :bio, length: { maximum: 2200 }
   validates :profile_picture, presence: true
   validates :password_digest, presence: true
