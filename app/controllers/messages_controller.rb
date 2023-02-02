777a03ad203ad174f53c7cca73c6301d06ac0380
chrismenke45 <chrismenke45@gmail.com>
2023-02-01 17:57:45 -0800

    began dialing in queries for index and show

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,13 @@ class MessagesController < ApplicationController
   before_action :authenticate_user
 
   def index
-    @messages = Message.all
+    @messages = Message.joins("JOIN users AS senders ON messages.sender_id = senders.id").joins("JOIN users AS receivers ON messages.receiver_id = receivers.id")
+      .select("messages.id, messages.created_at, messages.text")
+      .select("CASE WHEN messages.receiver_id = #{@current_user.id} THEN messages.sender_id ELSE messages.receiver_id END AS user_id")
+      .select("CASE WHEN messages.sender_id = #{@current_user.id} THEN receivers.profile_picture ELSE senders.profile_picture END")
+      .select("CASE WHEN messages.sender_id = #{@current_user.id} THEN receivers.username ELSE senders.username END")
+      .where("messages.sender_id = :current_user OR messages.receiver_id = :current_user", { current_user: @current_user.id })
+      .order("created_at DESC")
     render :json => @messages
   end
 
@@ -17,7 +23,7 @@ class MessagesController < ApplicationController
   end
 
   def show
-    @message.where("sender_id = ?", params[:sender_id])
+    @message.where("sender_id = :current_user AND receiver_id = :other_user OR sender_id = :other_user AND receiver_id = :current_user", { current_user: @current_user.id, other_user: params[:sender_id] })
     render :json => @message
   end
 
